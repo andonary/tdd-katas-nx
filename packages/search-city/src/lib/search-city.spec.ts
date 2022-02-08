e6d04a7f479826f25722f41540580b306cb42c3e
Ando Nary <ando.nary@gmail.com>
2022-02-08 08:31:42 +0100

    15 min

--- packages/search-city/src/lib/search-city.spec.ts
+++ packages/search-city/src/lib/search-city.spec.ts
@@ -1,7 +1,16 @@
 import { searchCity, cities } from './search-city';
 
 describe('searchCity', () => {
-  it('should work', () => {
-    expect(searchCity()).not.toBeDefined();
+  it('should search cities', () => {
+    expect(searchCity('')).toEqual([]);
+    expect(searchCity('P')).toEqual([]);
+    expect(searchCity('Pa')).toEqual([{ name: 'Paris' }]);
+    expect(searchCity('Va')).toEqual([
+      { name: 'Valencia' },
+      { name: 'Vancouver' },
+    ]);
+    expect(searchCity('pa')).toEqual([{ name: 'Paris' }]);
+    expect(searchCity('ape')).toEqual([{ name: 'Budapest' }]);
+    expect(searchCity('*')).toEqual(cities.map((name) => ({ name })));
   });
 });
--- packages/search-city/src/lib/search-city.ts
+++ packages/search-city/src/lib/search-city.ts
@@ -1,5 +1,17 @@
-export function searchCity(): any {
-  return undefined;
+function createResult(city: string = '') {
+  return { name: city };
+}
+
+function format(city: string) {
+  return city.toLowerCase();
+}
+
+export function searchCity(input: string): any {
+  if (input === '*') return cities.map((city) => createResult(city));
+  if (input.length < 2) return [];
+  return cities
+    .filter((city) => new RegExp(format(input)).test(format(city)))
+    .map((city) => createResult(city));
 }
 
 export const cities = [
